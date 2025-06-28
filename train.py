cd96f8d1109f39c22220745c77381ff6e8e5dd4a
ritam <ritamktpp@gmail.com>
2025-06-28 20:48:38 +0000

    Initial commit with IRIS pipeline and metrics

--- train.py
+++ train.py
@@ -7,22 +7,26 @@ import pickle
 
 
 data = pd.read_csv('iris.csv')
-
-train, test = train_test_split(data, test_size = 0.4, stratify = data['species'], random_state = 42)
-X_train = train[['sepal_length','sepal_width','petal_length','petal_width']]
+train, test = train_test_split(data, test_size=0.4, stratify=data['species'], random_state=42)
+X_train = train[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
 y_train = train.species
-X_test = test[['sepal_length','sepal_width','petal_length','petal_width']]
+X_test = test[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
 y_test = test.species
 
-print('training started ........')
-mod_dt = DecisionTreeClassifier(max_depth = 3, random_state = 1)
-mod_dt.fit(X_train,y_train)
-prediction=mod_dt.predict(X_test)
-print('training ended .......')
-print('The accuracy of the Decision Tree is',"{:.3f}".format(metrics.accuracy_score(prediction,y_test)))
-
+print('training started ...')
+mod_dt = DecisionTreeClassifier(max_depth=3, random_state=1)
+mod_dt.fit(X_train, y_train)
+prediction = mod_dt.predict(X_test)
+print('training ended ...')
+accuracy = metrics.accuracy_score(prediction, y_test)
+print('The accuracy of the Decision Tree is', "{:.3f}".format(accuracy))
 
 os.makedirs('models', exist_ok=True)
 with open('models/week2_model.pkl', 'wb') as f:
-    pickle.dump(mod_dt, f)
-print('Model saved')
+	pickle.dump(mod_dt, f)
+print('Model Saved')
+
+
+with open('metrics.txt', 'w') as f:
+	f.write(f"Test Accuracy: {accuracy:.3f}")
+print("Metrics saved")
